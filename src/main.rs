c1e6a163104d823fad23b431a28de14cd6cf8542
g <somewhere@outthere.com>
2017-07-05 21:33:20 -0500

    v 0.5

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,43 @@
-extern crate i3ipc;
-use i3ipc::I3EventListener;
-use i3ipc::Subscription;
-use i3ipc::event::Event;
+#![allow(dead_code)]
+
+extern crate nanomsg;
+extern crate clap;
+use clap::{App, SubCommand};
+mod focuswatcher;
+mod sockethandler;
+use std::thread;
+use std::sync::{Arc, Mutex};
+
+
+fn set_up_watch() {
+    println!("setup");
+    let workspace_list = Arc::new(Mutex::new(focuswatcher::WorkSpaceList::build()));
+    let c = workspace_list.clone();
+    let d = workspace_list.clone();
+
+    let fhandler = thread::spawn(move || focuswatcher::watch(c.as_ref()));
+    let rhandler = thread::spawn(move || sockethandler::receiver(d.as_ref()));
+
+    fhandler.join().unwrap();
+    rhandler.join().unwrap();
+}
+
 fn main() {
-  let mut listener = I3EventListener::connect().unwrap();
-
-  // subscribe to a couple events.
-  let subs = [Subscription::Mode, Subscription::Binding];
-  listener.subscribe(&subs).unwrap();
-
-  // handle them
-  for event in listener.listen() {
-      match event.unwrap() {
-          Event::ModeEvent(e) => println!("new mode: {}", e.change),
-          Event::BindingEvent(e) => println!("user input triggered command: {}", e.binding.command),
-          _ => unreachable!()
-      }
-  }
+    let matches = App::new("switch-it")
+        .version("1.0")
+        .author("Gavin Stringfellow")
+        .about("focus on different windows")
+        .args_from_usage("-w 'Switches to last focused window in last focused workspace'
+                          -c 'Switches to last focused window in current container'")
+        .subcommand(SubCommand::with_name("watch"))
+        .get_matches();
+    // focuswatcher::find_window_workspace_from_i3(33105776);
+    // set_up_watch();
+    if matches.is_present("watch") {
+        set_up_watch();
+    } else if matches.is_present("w") {
+        sockethandler::send(String::from("w"));
+    } else {
+        sockethandler::send(String::from("c"));
+    }
 }
---
+++ src/sockethandler.rs
@@ -0,0 +1,58 @@
+extern crate nanomsg;
+use nanomsg::{Socket, Protocol, PollRequest, PollInOut};
+use std::io::{Read, Write};
+use focuswatcher::WorkSpaceList;
+use std::sync::Mutex;
+
+static SOCKET_FILE: &str = "ipc:///tmp/switch-it.ipc";
+
+fn on_command(command: &String, workspace_list_mute: &Mutex<WorkSpaceList>) {
+    let ref wsl = workspace_list_mute.lock().unwrap();
+    if command.contains("w") {
+        // or c
+        wsl.last_workspace()
+    } else {
+        wsl.last_container()
+    }
+}
+
+
+pub fn receiver(workspace_list: &Mutex<WorkSpaceList>) {
+    let mut socket = Socket::new(Protocol::Pull).unwrap();
+    let _ = socket.bind(&SOCKET_FILE); // let _ = means we don't need any return value stored somewere
+    let mut request = String::new();
+
+    loop {
+        match socket.read_to_string(&mut request) {
+            Ok(_) => on_command(&request, workspace_list),
+            Err(err) => {
+                println!("failed '{}'", err);
+                break;
+            }
+        }
+        request.clear();
+    }
+}
+
+fn can_write_to_pipe(socket: &Socket) -> bool {
+    let mut pollfd = [socket.new_pollfd(PollInOut::Out)];
+    let mut poll_req = PollRequest::new(&mut pollfd);
+    let _ = Socket::poll(&mut poll_req, 10);
+    return poll_req.get_fds()[0].can_write();
+}
+
+pub fn send(msg: String) {
+    let mut socket = Socket::new(Protocol::Push).unwrap();
+    socket.connect(&SOCKET_FILE).unwrap();
+
+    if can_write_to_pipe(&socket) {
+        match socket.write_all(&msg.as_bytes()) {
+            Ok(_) => println!("SENDING '{}'", &msg),
+            Err(err) => {
+                println!("failed '{}'", err);
+            }
+        }
+    } else {
+        println!("nope");
+    }
+}
